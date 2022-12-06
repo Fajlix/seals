0db7309df1a0337416fca6642d3a2d8bda265f2d
ronjakh <ronjah@student.chalmers.se>
2022-12-06 16:45:55 +0100

    Added individual mass and size to agents, added no_of_agents and window size variables in main.py

--- agents.py
+++ agents.py
@@ -1,45 +1,59 @@
 import numpy as np
+from numpy import random
 '''
 Defines different agents e.g, human, maybe in the future more agent e.g police / attacker etc.
 '''
+
+
 class Agent:
     ''' Defines a agent'''
-    def __init__(self,x,y,mass=70,p_max =1,area =1,health = 100):
-        self.position = np.array([float(x),float(y)])
-        self.mass = mass
+    def __init__(self, x, y, individual_mass, individual_size, mass=70, p_max=1, area=1, health=100):
+        self.position = np.array([float(x), float(y)])
+        if individual_mass:
+            self.mass = random.normal(loc=70, scale=10)  # Mean = 70kg, Std = 10kg, feel free to change
+        else:
+            self.mass = mass
         self.velocity = np.array([0.0, 0.0])
         self.acceleration = np.array([0.0, 0.0])
-        self.external_force = np.array([0.0,0.0])
+        self.external_force = np.array([0.0, 0.0])
         # Towards ex stage, exit, etc
         self.attractive_force_magnitude = 0
         self.attraction_angle = 0
-        #For example friction, other
-        self.internal_force = np.array([0.0,0.0])
+        # For example friction, other
+        self.internal_force = np.array([0.0, 0.0])
         self.p_max = p_max
         # Area of lung for pressure
         self.area = area
         self.alive = True
-        #Instead of timer I though we could use health. Ex, we lose health over time depending on how high the pressure is
+        # Instead of timer I though we could use health. Ex, we lose health over time depending on how high the pressure is
         # Or how many is in its radius. 
         self.health = health
         self.max_health = health
         # Just some starting ideas, size could be used randomly 
         # so that every agent is not the same size, like Viktor is two times the
         # size of me (Felix) and will probably survive longer
-        self.size = 2
+        if individual_size and individual_mass:
+            r = random.normal(loc=0.006, scale=0.001)  # Chosen s.t. 70*0.006=0.42, std arbitrary
+            self.size = r * self.mass
+        elif individual_size:
+            self.size = random.normal(loc=0.42, scale=0.05)  # Mean = 42cm, Std = 5cm, feel free to change
+        else:
+            self.size = 2  # We have giants among us
         # I was thinking that depending on the type of person their reactions to events might differ, 
         self.behaviour = "stressed"
-    def apply_pressure(self,dt):
+
+    def apply_pressure(self, dt):
         if(np.linalg.norm(self.external_force/self.area) > self.p_max):
-            #TODO DAMAGE SHOULD NOT BE A CONSTANT!
+            # TODO DAMAGE SHOULD NOT BE A CONSTANT!
             damage = 1
             self.health -= damage
         elif(self.health < self.max_health):
-            #TODO HEALING SHOULD NOT BE A CONSTANT!
+            # TODO HEALING SHOULD NOT BE A CONSTANT!
             healing = 0.1
             healing = np.min([self.health + healing,self.max_health])
-    def step(self,dt):
-        self.attraction_towards_stage((500,500), 5)
+
+    def step(self, dt):
+        self.attraction_towards_stage((500, 500), 5)
         self.acceleration = (self.external_force + self.internal_force)/self.mass
         self.velocity += self.acceleration*dt
         # set max velocity in x and y direction
@@ -48,6 +62,7 @@ class Agent:
         
         self.position += self.velocity*dt
         self.apply_pressure(dt)
+
     def attraction_towards_stage(self, point_of_attraction, attractive_force_magnitude):
         # get the angle between the agent and the point of attraction
         self.attraction_angle = np.arctan2(point_of_attraction[1] - self.position[1], point_of_attraction[0] - self.position[0])
--- main.py
+++ main.py
@@ -8,22 +8,32 @@ from crowdPhysics import *
 from environment import *
 import random
 import multiprocessing as mp
+
+# Booleans:
+agent_individual_mass = False
+agent_individual_size = False
+
+# Variables
+no_of_agents = 300
+height_of_window, width_of_window = 1000, 1000
+
+
 def run():
     agents = []
     
-    for i in range(1000):
-        x = random.randint(8/2, 1000 - 8/2)
-        y = random.randint(8/2, 1000 - 8/2)
+    for i in range(no_of_agents):
+        x = random.randint(8/2, width_of_window - 8/2)
+        y = random.randint(8/2, height_of_window - 8/2)
         # make sure that the agents are not in radius of each other
-        while any([np.sqrt(np.power(x - agent.position[0],2) + np.power(y - agent.position[1],2)) < 16 for agent in agents]):
-            x = random.randint(8/2, 1000 - 8/2)
-            y = random.randint(8/2, 1000 - 8/2)
+        while any([np.sqrt(np.power(x - agent.position[0], 2) + np.power(y - agent.position[1], 2)) < 16 for agent in agents]):
+            x = random.randint(8/2, width_of_window - 8/2)
+            y = random.randint(8/2, height_of_window - 8/2)
         
-        agents.append(Agent(x,y))
+        agents.append(Agent(x, y, agent_individual_mass, agent_individual_size))
 
     env = Environment(agents)
 
-    graphic = Graphics(1000, 1000)
+    graphic = Graphics(width_of_window, height_of_window)
 
     graphic.drawHuman(env.getAgentPositions())
     running = True
@@ -34,10 +44,11 @@ def run():
             if event.type == pygame.QUIT:
                 running = False
 
-
         env.update()
         if env.time - last_update_time > 0.5:
             graphic.drawHuman(env.getAgentPositions())
             last_update_time = env.time
+
+
 if __name__ == "__main__":
     run()
